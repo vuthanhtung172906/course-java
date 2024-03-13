8753dd8dda8c48f12acd366ca0b654988b3e470a
vuthanhtung1998 <vuthanhtung1999@gmail.com>
2024-03-13 10:08:00 +0700

    add test api

--- src/main/java/com/example/demo/SareetaApplication.java
+++ src/main/java/com/example/demo/SareetaApplication.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
@@ -23,5 +26,6 @@ public class SareetaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SareetaApplication.class, args);
 	}
+	
 
 }
--- src/main/java/com/example/demo/security/WebSecurityConfiguration.java
+++ src/main/java/com/example/demo/security/WebSecurityConfiguration.java
@@ -28,6 +28,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.GET, "/api/test").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
